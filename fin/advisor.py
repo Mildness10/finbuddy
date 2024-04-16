d92035023c4bb60d76648c48e5913d18bfaf363d
Mildness10 <m.akomoize@gmail.com>
2024-04-15 22:23:28 -0400

    added secret keys to gitignore

--- fin/advisor.py
+++ fin/advisor.py
@@ -2,6 +2,7 @@ import PyPDF2
 from langchain_openai import ChatOpenAI
 import os
 import glob
+from dotenv import load_dotenv
 import warnings 
 warnings.filterwarnings("ignore")
 
@@ -22,7 +23,8 @@ pdf_file_names = [os.path.basename(pdf_file) for pdf_file in pdf_files]
 
 pdf_texts = [extract_text_from_pdf(pdf_path) for pdf_path in pdf_file_names]
 
-llm = ChatOpenAI(api_key="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")
+load_dotenv()
+llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 
 def get_advisor_response(user_input):
